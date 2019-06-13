089f48eb059e79f6be07620115302c0f29ec35b3
Aokromes <aokromes@gmail.com>
2019-06-14 00:19:08 +0200

    DB/Quest:  Misc Gilneas quest fixes By Malcrom

--- src/server/scripts/EasternKingdoms/Gilneas/chapter2.cpp
+++ src/server/scripts/EasternKingdoms/Gilneas/chapter2.cpp
@@ -24,6 +24,7 @@
 #include "SpellScript.h"
 #include "SpellInfo.h"
 #include "SpellAuras.h"
+#include "World.h"
 
 /*######
 ##Quest 14348 - You Can't Take 'Em Alone
@@ -58,7 +59,65 @@ class spell_gilneas_horrid_abomination_explosion : public SpellScript
     }
 };
 
+/*######
+##Quest 14368 - Save the Children!
+######*/
+
+// 68596 Gilneas - Quest - Save James
+class spell_gilneas_quest_save_james : public SpellScript
+{
+    PrepareSpellScript(spell_gilneas_quest_save_james);
+
+    void HandleScriptEffect(SpellEffIndex /*effIndex*/)
+    {
+        if (Player* player = GetCaster()->ToPlayer())
+            player->Talk(GetSpellInfo()->Effects[1].BasePoints, CHAT_MSG_SAY, sWorld->getFloatConfig(CONFIG_LISTEN_RANGE_SAY), player);
+    }
+
+    void Register() override
+    {
+        OnEffectHitTarget += SpellEffectFn(spell_gilneas_quest_save_james::HandleScriptEffect, EFFECT_1, SPELL_EFFECT_SCRIPT_EFFECT);
+    }
+};
+
+// 68597 Gilneas - Quest - Save Cynthia
+class spell_gilneas_quest_save_cynthia : public SpellScript
+{
+    PrepareSpellScript(spell_gilneas_quest_save_cynthia);
+
+    void HandleScriptEffect(SpellEffIndex /*effIndex*/)
+    {
+        if (Player* player = GetCaster()->ToPlayer())
+            player->Talk(GetSpellInfo()->Effects[0].BasePoints, CHAT_MSG_SAY, sWorld->getFloatConfig(CONFIG_LISTEN_RANGE_SAY), player);
+    }
+
+    void Register() override
+    {
+        OnEffectHitTarget += SpellEffectFn(spell_gilneas_quest_save_cynthia::HandleScriptEffect, EFFECT_0, SPELL_EFFECT_SCRIPT_EFFECT);
+    }
+};
+
+// 68598 Gilneas - Quest - Save Ashley
+class spell_gilneas_quest_save_ashley : public SpellScript
+{
+    PrepareSpellScript(spell_gilneas_quest_save_ashley);
+
+    void HandleScriptEffect(SpellEffIndex /*effIndex*/)
+    {
+        if (Player* player = GetCaster()->ToPlayer())
+            player->Talk(GetSpellInfo()->Effects[0].BasePoints, CHAT_MSG_SAY, sWorld->getFloatConfig(CONFIG_LISTEN_RANGE_SAY), player);
+    }
+
+    void Register() override
+    {
+        OnEffectHitTarget += SpellEffectFn(spell_gilneas_quest_save_ashley::HandleScriptEffect, EFFECT_0, SPELL_EFFECT_SCRIPT_EFFECT);
+    }
+};
+
 void AddSC_gilneas_c2()
 {
     RegisterSpellScript(spell_gilneas_horrid_abomination_explosion);
+    RegisterSpellScript(spell_gilneas_quest_save_james);
+    RegisterSpellScript(spell_gilneas_quest_save_cynthia);
+    RegisterSpellScript(spell_gilneas_quest_save_ashley);
 }
