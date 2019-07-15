ad802758c422e43ba95f55ba02c326b6b9f2c86e
Aokromes <aokromes@gmail.com>
2019-07-15 12:39:42 +0200

    rename files

--- sql/updates/world/4.3.4/2019_07_14_06_world_was_2019_07_12_00_world.sql
+++ sql/updates/world/4.3.4/2019_07_14_06_world_was_2019_07_12_00_world.sql
@@ -65,9 +65,11 @@ DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=13 AND `SourceEntry`=42
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`,`SourceGroup`,`SourceEntry`,`SourceId`,`ElseGroup`,`ConditionTypeOrReference`,`ConditionTarget`,`ConditionValue1`,`ConditionValue2`,`ConditionValue3`,`NegativeCondition` ,`ErrorTextId`,`ScriptName`,`Comment`) VALUES
 (13,1,42442,0,1,31,0,3,24921,0,0,0,'','Vengeance Landing Cannonfire'),
 (13,1,42442,0,0,31,0,3,23803,0,0,0,'','Vengeance Landing Cannonfire');
+/*
 DELETE FROM `creature_template_movement` WHERE `CreatureId` IN (23805);
 INSERT INTO `creature_template_movement` (`CreatureId`, `Ground`, `Swim`, `Flight`, `Rooted`) VALUES
 (23805, 0, 0, 1, 0);
+*/
 UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry` IN (23805,23968); 
 DELETE FROM `smart_scripts` WHERE `entryorguid` IN (23805,23968) AND `source_type`=0;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
