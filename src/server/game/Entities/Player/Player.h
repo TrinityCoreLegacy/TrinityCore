5a10c166963d20400c6506394150fa724044a5f3
ReyDonovan <gennady.zamalaev@gmail.com>
2020-05-04 10:36:58 +0300

    Revert "Core/Currency: Implement currency to reputation conversion"
    This reverts commit 7fa3ee6534000c4e349c4e3a918f34f4103c2fb1.

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -1251,7 +1251,7 @@ class TC_GAME_API Player : public Unit, public GridObject<Player>
         /// return presence related currency
         bool HasCurrency(uint32 id, uint32 count) const;
         /// initialize currency count for custom initialization at create character
-        void SetCreateCurrency(uint32 id, uint32 count, bool printLog = true);
+        void SetCurrency(uint32 id, uint32 count, bool printLog = true);
         void ResetCurrencyWeekCap();
 
         /**
